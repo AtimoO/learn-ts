1e9b08cb76a96d418eb8edf21f5173aff5e701a1
A. Timokhin <timoha.job@gmail.com>
2022-07-05 20:23:42 +0300

    tuple-array-readonly

--- index.ts
+++ index.ts
@@ -8,20 +8,21 @@ const obj: { pages: number } = { // obj
 
 const arrNum: number[] = [1, 3, 4]; // for array, all number
 const arrStr: string[] = ["1", "2"];
+const arr: number[][] = [[1, 11, 101], [2, 22], [3]]; // многомерный
 
 // Только задные типы для каждого свой
 const tuple: [number, string] = [2, "3"]; // tuple
 
 let any: any = true; // Любой тип
  */
-
+/*
 function getName(obj: { name: string, surname: string }): string {
   return `${obj.name} ${obj.surname}`
 }
 
-// const getFullName = (name: string, surname: string): string => {
-//   return `${name} ${surname}`
-// }
+const getFullName = (name: string, surname: string): string => {
+  return `${name} ${surname}`
+}
 
 const userObj: { name: string, surname: string, city: string, age: number } = {
   name: "Name",
@@ -30,4 +31,27 @@ const userObj: { name: string, surname: string, city: string, age: number } = {
   age: 24,
 }
 
+const printGreeting = (name: string): void => { // пустой
+  console.log(`Hi, ${name}`);
+};
+
+//Literal Types
+let position: 'absolute' | 'fixed' | 'relative' | 'static' | 'inherit';
+
 console.log(getName(userObj));
+
+ */
+/*
+const arrType: string[] = ["ASC", "DESC", "DEF"];
+const arrType1: ReadonlyArray<string> = ["ASC", "DESC", "DEF"];
+
+const rez: string[] = arrType.filter((type: string) => type !== "ASC");
+
+const typl: [number, string] = [2, "asd"]; // pop-push работает
+
+const arrSp: readonly [number, string, ...boolean[]] = [1, "asd", false, true];
+
+console.log(rez);
+*/
+
+
